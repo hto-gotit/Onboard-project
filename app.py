3527b30e0de8549b8732e8a77cf9bcfb2d5a6e09
hto-gotit <88703082+hto-gotit@users.noreply.github.com>
2021-08-11 17:04:03 +0700

    feat: Changed from sqlite to mysql

--- app.py
+++ app.py
@@ -1,23 +1,36 @@
 from flask import Flask
-from flask.json import jsonify
-from flask_jwt_extended.view_decorators import jwt_required
 from flask_restful import Api
-import mysql.connector
 from flask_jwt_extended import JWTManager
 
-app = Flask (__name__)
+from resources.users import UserRegister, User
+from resources.login import UserLogin
+from resources.items import Item, ItemList, ItemCreate
+from resources.categories import Category, CategoryList
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Password1!@localhost/catalogdb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
-app.secret_key = 'Project_onboard!@#'
+app.secret_key = 'SecRetPassWord123*()'
 api = Api(app)
 
 @app.before_first_request
-def create_table():
+def create_tables():
     db.create_all()
+
+
+jwt = JWTManager(app)
+
+api.add_resource(ItemList, '/categories/<int:category_id>/items')
+api.add_resource(Item, '/categories/<int:category_id>/items/<int:item_id>')
+api.add_resource(ItemCreate, '/categories/<int:category_id>/items')
+api.add_resource(CategoryList, '/categories')
+api.add_resource(Category, '/categories/<int:category_id>')
+api.add_resource(UserRegister, '/users')
+api.add_resource(User, '/user/<int:user_id>')
+api.add_resource(UserLogin, '/login')
 
 if __name__ == '__main__':
     from db import db
     db.init_app(app)
-    app.run(port=5000)
+    app.run(port=5000, debug=True)
