ede00d8bbb35d5bf3448ff68a81d40c7ceb9fe63
hto-gotit <88703082+hto-gotit@users.noreply.github.com>
2021-08-11 16:25:08 +0700

    feat: Added columns
    Added creation/update date

--- models/users.py
+++ models/users.py
@@ -3,17 +3,31 @@ from db import db
 
 class UserModel(db.Model):
     __tablename__ = 'user'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    username = db.Column(db.String(80), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
     created = db.Column(db.DateTime(timezone=True), server_default=db.func.now())
-    updated = db.Column(db.DateTime(timezone=True), onupdate=db.func.now())
-    items = db.relationship('ItemModel', backref='user', lazy=True)
+    updated = db.Column(db.DateTime(timezone=True), server_default=db.func.now(), onupdate=db.func.now())
 
     def __init__(self, username, password):
         self.username = username
         self.password = password
 
+    def json(self):
+        return {'id': self.id, 'username': self.username}
+
     def save_to_db(self):
         db.session.add(self)
-        db.commit()
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    @classmethod
+    def find_by_username(cls, username):
+        return cls.query.filter_by(username=username).first()
+
+    @classmethod
+    def find_by_id(cls, _id):
+        return cls.query.filter_by(id=_id).first()
