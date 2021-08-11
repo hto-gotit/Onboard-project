54b6b644c2e018a8f759c1af7d53fc7e7dcedaa3
hto-gotit <88703082+hto-gotit@users.noreply.github.com>
2021-08-11 16:23:14 +0700

    feat: Wrote finding functions and added columns

--- models/categories.py
+++ models/categories.py
@@ -1,19 +1,32 @@
 from db import db
-import datetime
 
 
 class CategoryModel(db.Model):
     __tablename__ = 'category'
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(20), unique=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.String(80), nullable=False)
     created = db.Column(db.DateTime(timezone=True), server_default=db.func.now())
-    updated = db.Column(db.DateTime(timezone=True), onupdate=db.func.now())
-    items = db.relationship('ItemModel', backref='category', lazy=True)
+    updated = db.Column(db.DateTime(timezone=True), server_default=db.func.now(), onupdate=db.func.now())
+    items = db.relationship('ItemModel', lazy='dynamic')
 
     def __init__(self, name):
         self.name = name
 
+    def json(self):
+        return {'id': self.id, 'name': self.name, 'create': str(self.created), 'update': str(self.updated), 'items': [item.json() for item in self.items.all()]}
+
+    @classmethod
+    def find_by_id(cls, category_id):
+        return cls.query.filter_by(id=category_id).first()
+
+    @classmethod
+    def find_all(cls):
+        return cls.query.all()
+
     def save_to_db(self):
         db.session.add(self)
-        db.commit()
+        db.session.commit()
 
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
