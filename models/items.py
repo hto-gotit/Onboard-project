590cd27ed7549e70c825ccf2e7edd55c120120ea
hto-gotit <88703082+hto-gotit@users.noreply.github.com>
2021-08-11 16:24:17 +0700

    feat: Wrote finding functions and added columns
    Added Created/Updated columns

--- models/items.py
+++ models/items.py
@@ -3,14 +3,14 @@ from db import db
 
 class ItemModel(db.Model):
     __tablename__ = 'item'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    description = db.Column(db.String, nullable=True)
-    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    created = db.Column(db.DateTime(timezone=True), server_default=db.func.now())
-    updated = db.Column(db.DateTime(timezone=True), onupdate=db.func.now())
 
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(200), nullable=True)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    created = db.Column(db.DateTime(timezone=True), server_default=db.func.now())
+    updated = db.Column(db.DateTime(timezone=True), server_default=db.func.now(), onupdate=db.func.now())
     category = db.relationship('CategoryModel')
     user = db.relationship('UserModel')
 
@@ -19,6 +19,18 @@ class ItemModel(db.Model):
         self.description = description
         self.category_id = category_id
         self.user_id = user_id
+
+    def json(self):
+        return {'id': self.id, 'name': self.name, 'description': self.description, 'category_id': self.category_id,
+                'user_id': self.user_id, 'create': str(self.created), 'update': str(self.updated)}
+
+    @classmethod
+    def find_by_id(cls, item_id):
+        return cls.query.filter_by(id=item_id).first()
+
+    @classmethod
+    def find_all(cls):
+        return cls.query.all()
 
     def save_to_db(self):
         db.session.add(self)
