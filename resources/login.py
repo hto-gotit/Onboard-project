31070501c2bbd1598f5ceae82aa5dbc30cb43426
hto-gotit <88703082+hto-gotit@users.noreply.github.com>
2021-08-12 15:51:12 +0700

    feat: Added a function to verify hashed password

--- resources/login.py
+++ resources/login.py
@@ -1,22 +1,33 @@
-from flask import request
-from flask_jwt_extended import create_access_token
-from flask_restful import Resource
+from flask import request                               # import flask request
+from flask_jwt_extended import create_access_token      # import needed jwt functions
+from flask_restful import Resource                      # import resource from restful
+import bcrypt                                           # import bcrypt for security use
 
-from models.users import UserModel
+from models.users import UserModel                      # import user models
 
 
+# /auth/login resource
 class UserLogin(Resource):
+    # Function to log the user in
     @classmethod
     def post(cls):
-        data = request.json
-
-        if 'username' not in data:
-            return {'message': 'Lacking field username'}, 400
-        user = UserModel.find_by_username(data['username'])
-        if user and user.password == data['password']:
-            try:
+        data = request.json                                         # fetch the request data
+        if 'username' not in data:                                  # return 400 if the request does not have
+            return {'message': 'Lacking field username'}, 400       # username key
+        user = UserModel.find_by_username(data['username'])         # get the user from database
+        if not user:
+            return {'message': 'Invalid username'}, 400             # return 400 if username is invalid
+        if user and verify_password(data['password'], user.password):     # if user exists and password is correct,
+            try:                                                          # then return a jwt access token
                 access_token = create_access_token(identity=user.id, fresh=True)
                 return {'access_token': access_token}, 200
             except:
-                return {'message': 'Internal error, login unsuccessful'}, 500
-        return {'message': 'Invalid credentials'}, 400
+                return {'message': 'Internal error, login unsuccessful'}
+        return {'message': 'Invalid password'}, 400                      # Otherwise, password is wrong
+
+
+# Function to verify user request password for login and hashed password in the database
+def verify_password(password, database_hash):
+    pass_bytes = password.encode()                      # encode the request password
+    hash_bytes = database_hash.encode()                 # encode the database hashed password
+    return bcrypt.checkpw(pass_bytes, hash_bytes)       # return true if they are the same, false otherwise
