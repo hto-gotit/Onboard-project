56507413f2577316e7bb53fcdbb30c95d0ff9068
hto-gotit <88703082+hto-gotit@users.noreply.github.com>
2021-08-12 15:49:27 +0700

    docs: Added comments

--- schemas/users.py
+++ schemas/users.py
@@ -1,24 +1,27 @@
 from marshmallow import Schema, fields, ValidationError, validates     # import Marshmallow
 
 
+# Declare the Schema for User, which consists of user's username and password
 class UserSchema(Schema):
-    username = fields.String(required=True)
-    password = fields.String(required=True)
+    username = fields.String(required=True)         # username of the account
+    password = fields.String(required=True)         # password of the account
 
+    # Function to validate the username of the account
     @validates("username")
     def validate_username(self, value):
         if type(value) != str:
-            raise ValidationError("Username must be a string.")
-        if len(value) < 1:
+            raise ValidationError("Username must be a string.")                   # The username must be a string
+        if len(value) < 1:                                                        # with length in range [1,79]
             raise ValidationError("Username's length must be greater than 0.")
         if len(value) > 79:
             raise ValidationError("Username's length must be less than 80.")
 
+    # Function to validate the password of the account
     @validates("password")
     def validate_password(self, value):
         if type(value) != str:
-            raise ValidationError("Password must be a string.")
-        if len(value) < 1:
+            raise ValidationError("Password must be a string.")                    # The password must be a string
+        if len(value) < 1:                                                         # with length in range [1,79]
             raise ValidationError("Password's length must be greater than 0.")
         if len(value) > 79:
             raise ValidationError("Password's length must be less than 80.")
