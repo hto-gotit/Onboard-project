787a798fc3573e4c3e69cc380cefa59c8b85598c
hto-gotit <88703082+hto-gotit@users.noreply.github.com>
2021-08-12 15:52:47 +0700

    docs: Added comments

--- resources/categories.py
+++ resources/categories.py
@@ -1,37 +1,51 @@
-from flask_restful import Resource
-from models.categories import CategoryModel
-from schemas.categories import CategorySchema
+from flask_restful import Resource                  # import resource from restful
+
+from models.categories import CategoryModel         # import model of category
+from schemas.categories import CategorySchema       # import schema of category
 
 
+# /categories resource
 class CategoryList(Resource):
-    def get(self):
-        category_list_schema = CategorySchema(many=True)
+    # Function to get all of the available categories
+    @classmethod
+    def get(cls):
+        category_list_schema = CategorySchema(many=True)            # declare the Schemas of categories
         try:
-            return category_list_schema.dump(CategoryModel.find_all()), 200
+            return category_list_schema.dump(CategoryModel.find_all()), 200     # try getting all categories
         except:
             return {'message': 'Cannot get categories list, internal error'}, 500
 
 
+# /categories/<category_id> resource (not available to users)
 # class Category(Resource):
-#     def get(self, category_id):
+#     # Function to get the information of a specified category
+#     @classmethod
+#     def get(cls, category_id):
 #         category = CategoryModel.find_by_id(category_id)
 #         category_schema = CategorySchema()
 #
 #         if category:
-#             return category.json()
+#             return category_schema.dump(category), 200
 #         return {'message': 'Category not found'}, 404
 #
-#     def post(self, category_id):
+#     # Function to create a category
+#     @classmethod
+#     def post(cls, category_id):
 #         if CategoryModel.find_by_id(category_id):
 #             return {'message' : "Category '{}' already exists".format(category_id)}, 400
 #         category = CategoryModel('CatNo' + str(category_id))
-#         # try:
-#         category.save_to_db()
-#         # except:
-#         #     return {'message' : 'Error creating category'}, 500
-#         return category.json(), 201
+#         category_schema = CategorySchema()
 #
-#     def delete(self, category_id):
+#         try:
+#             category.save_to_db()
+#         except:
+#             return {'message' : 'Error creating category'}, 500
+#
+#         return category_schema.dump(category), 201
+#
+#     # Function to delete a category
+#     @classmethod
+#     def delete(cls, category_id):
 #         category = CategoryModel.find_by_id(category_id)
 #         if category:
 #             category.delete_from_db()
