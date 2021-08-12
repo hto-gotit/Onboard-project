613c6f49069c719042d779580e90b07bf38ddcd6
hto-gotit <88703082+hto-gotit@users.noreply.github.com>
2021-08-12 15:50:01 +0700

    docs: Added comments

--- resources/users.py
+++ resources/users.py
@@ -1,35 +1,39 @@
-from flask import request, jsonify
-from flask_restful import Resource, reqparse
-from marshmallow import ValidationError
+from flask import request                   # import flask request
+from flask_restful import Resource          # import resource from restful
+from marshmallow import ValidationError     # import class to raise errors for marshmallow
 
-from models.users import UserModel
-from schemas.users import UserSchema
+from models.users import UserModel          # import model for user
+from schemas.users import UserSchema        # import schema for user
 
 
+# /users resource, used only for registering a new user
 class UserRegister(Resource):
+    # Function to create a new user
+    @classmethod
     def post(self):
-        data = request.json
-
-        user_schema = UserSchema()
+        data = request.json                     # fetch the info of the request
+        user_schema = UserSchema()              # declare the schema for user
         try:
-            user_schema.load(data)
+            user_schema.load(data)              # validate the request
         except ValidationError as err:
             return err.messages
 
-        if UserModel.find_by_username(data['username']):
+        if UserModel.find_by_username(data['username']):            # if username already existed, return 400
             return {'message': 'Username already exists'}, 400
 
-        user = UserModel(**data)
+        user = UserModel(**data)                # username available, create a new user
 
         try:
-            user.save_to_db()
+            user.save_to_db()                   # save the user to the database
         except:
             return {'message': 'Internal error, user not created'}, 500
 
         return {'message': 'User created successfully'}, 201
 
 
+# /user/user_id resource (Not available to users)
 # class User(Resource):
+#     # Function to get id and usernam of an user
 #     @classmethod
 #     def get(cls, user_id):
 #         user = UserModel.find_by_id(user_id)
@@ -38,6 +42,7 @@ class UserRegister(Resource):
 #         user_schema = UserSchema()
 #         return user_schema.dump(user)
 #
+#     # Function to delete an user
 #     @classmethod
 #     def delete(cls, user_id):
 #         user = UserModel.find_by_id(user_id)
