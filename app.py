c5e078adfc7fcdc6d53b93381594193990cc4096
hto-gotit <88703082+hto-gotit@users.noreply.github.com>
2021-08-12 11:19:46 +0700

    refactor: Commented out unused 'Category' and 'User' classes and their endpoints

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 
-from resources.users import UserRegister, User
+from resources.users import UserRegister
+# from resources.users import User
 from resources.login import UserLogin
 from resources.items import Item, ItemList, ItemCreate
-from resources.categories import Category, CategoryList
+from resources.categories import CategoryList
+# from resources.categories import Category
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Password1!@localhost/catalogdb'
@@ -25,9 +27,9 @@ api.add_resource(ItemList, '/categories/<int:category_id>/items')
 api.add_resource(Item, '/categories/<int:category_id>/items/<int:item_id>')
 api.add_resource(ItemCreate, '/categories/<int:category_id>/items')
 api.add_resource(CategoryList, '/categories')
-api.add_resource(Category, '/categories/<int:category_id>')
+# api.add_resource(Category, '/categories/<int:category_id>')
 api.add_resource(UserRegister, '/users')
-api.add_resource(User, '/user/<int:user_id>')
+# api.add_resource(User, '/user/<int:user_id>')
 api.add_resource(UserLogin, '/login')
 
 if __name__ == '__main__':
