7bfa9b24015e658a604f449e5c2540ad977b66c1
hto-gotit <88703082+hto-gotit@users.noreply.github.com>
2021-08-13 12:21:45 +0700

    style: reformatted to PEP8

--- schemas/users.py
+++ schemas/users.py
@@ -1,4 +1,5 @@
-from marshmallow import Schema, fields, ValidationError, validates     # import Marshmallow
+# import Marshmallow
+from marshmallow import Schema, fields, ValidationError, validates
 
 
 # Declare the Schema for User, which consists of user's username and password
@@ -9,19 +10,21 @@ class UserSchema(Schema):
     # Function to validate the username of the account
     @validates("username")
     def validate_username(self, value):
+        # The username must be a string with length in range [1,79]
         if type(value) != str:
-            raise ValidationError("Username must be a string.")                   # The username must be a string
-        if len(value) < 1:                                                        # with length in range [1,79]
-            raise ValidationError("Username's length must be greater than 0.")
+            raise ValidationError("Username must be a string.")
+        if len(value) < 1:
+            raise ValidationError("Username's length must be > 0.")
         if len(value) > 79:
-            raise ValidationError("Username's length must be less than 80.")
+            raise ValidationError("Username's length must be < 80.")
 
     # Function to validate the password of the account
     @validates("password")
     def validate_password(self, value):
+        # The password must be a string with length in range [1,79]
         if type(value) != str:
-            raise ValidationError("Password must be a string.")                    # The password must be a string
-        if len(value) < 1:                                                         # with length in range [1,79]
-            raise ValidationError("Password's length must be greater than 0.")
+            raise ValidationError("Password must be a string.")
+        if len(value) < 1:
+            raise ValidationError("Password's length must be > 0.")
         if len(value) > 79:
-            raise ValidationError("Password's length must be less than 80.")
+            raise ValidationError("Password's length must be < 80.")
