10c14df3a2fefd3259b970b074add17895243236
hto-gotit <88703082+hto-gotit@users.noreply.github.com>
2021-08-13 12:21:09 +0700

    style: reformatted to PEP8

--- schemas/categories.py
+++ schemas/categories.py
@@ -1,17 +1,22 @@
-from marshmallow import Schema, fields, validates, ValidationError     # import Marshmallow
+# import Marshmallow
+from marshmallow import Schema, fields, validates, ValidationError
 
 
 # Declare the Schema for Category, which consists of category id and its name
 class CategorySchema(Schema):
-    id = fields.Integer()                       # id of the category
-    name = fields.String(required=True)         # name of the category, it is required
+    # id of the category
+    id = fields.Integer()
+    # name of the category, it is required
+    name = fields.String(required=True)
 
     # function to validate the name of the category
     @validates("name")
     def validate_username(self, value):
-        if type(value) != str:                                                       # name must be a string
+        # name must be a string
+        if type(value) != str:
             raise ValidationError("Category name must be a string.")
-        if len(value) < 1:                                                           # name length must be in the
-            raise ValidationError("Category name's length must be greater than 0.")  # range of [1,19]
+        # name length must be in the range of [1,19]
+        if len(value) < 1:
+            raise ValidationError("Category name's length must be > 0.")
         if len(value) > 19:
-            raise ValidationError("Category name's length must be less than 20.")
+            raise ValidationError("Category name's length must be < 20.")
