1b42a8386faba2d82adf8a9895b354e3ff862030
hto-gotit <88703082+hto-gotit@users.noreply.github.com>
2021-08-13 12:24:36 +0700

    style: reformatted to PEP8

--- app.py
+++ app.py
@@ -1,25 +1,51 @@
-from flask import Flask                                     # import Flask class
-from flask_restful import Api                               # import restful API
-from flask_jwt_extended import JWTManager                   # import JWT manager
+import sqlalchemy.exc
+from flask import Flask                               # import Flask class
+from flask_restful import Api                         # import restful API
+from flask_jwt_extended import JWTManager             # import JWT manager
 
-from resources.users import UserRegister                    # import resource to register
-# from resources.users import User                          # import User resource (not available to users)
-from resources.login import UserLogin                       # import resource to login
-from resources.items import Item, ItemList, ItemLatests     # import all item-related resource
-from resources.categories import CategoryList               # import resource to get all categories
-# from resources.categories import Category                 # import Category resource (not available to users)
+# import resource to register
+from resources.users import UserRegister
+# import User resource (not available to users)
+# from resources.users import User
+# import resource to login
+from resources.login import UserLogin
+# import all item-related resource
+from resources.items import Item, ItemList, ItemLatests
+# import resource to get all categories
+from resources.categories import CategoryList
+# import Category resource (not available to users)
+# from resources.categories import Category
+from models.categories import CategoryModel
 
-app = Flask(__name__)                                       # Instantiate the Flask application
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Password1!@localhost/catalogdb'  # path to database
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False        # Disable tracking modifications
-app.config['PROPAGATE_EXCEPTIONS'] = True                   # Enable tracking exceptions
-app.secret_key = 'SecRetPassWord123*()'                     # Secret key for app
-api = Api(app)                                              # Instantiate the API
+# Instantiate the Flask application
+app = Flask(__name__)
+# path to database
+app.config['SQLALCHEMY_DATABASE_URI'] \
+    = 'mysql+pymysql://root:Password1!@localhost/catalogdb'
+# Disable tracking modifications
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Enable tracking exceptions
+app.config['PROPAGATE_EXCEPTIONS'] = True
+# Secret key for app
+app.secret_key = 'SecRetPassWord123*()'
+# Instantiate the API
+api = Api(app)
 
 
 @app.before_first_request               # Create all tales in database
 def create_tables():
-    db.create_all()
+    db.drop_all()                   # drop all past tables
+    db.create_all()                 # create all tables
+    # Create some default category
+    try:
+        cat1 = CategoryModel("First category")
+        cat2 = CategoryModel("Second category")
+        cat3 = CategoryModel("Third category")
+        cat1.save_to_db()
+        cat2.save_to_db()
+        cat3.save_to_db()
+    except sqlalchemy.exc.IntegrityError:
+        pass
 
 
 jwt = JWTManager(app)                   # Instantiate the JWT manager
