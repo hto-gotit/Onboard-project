7959729eff22dce7ebf9dc18926f139c945e08be
hto-gotit <88703082+hto-gotit@users.noreply.github.com>
2021-08-13 12:22:12 +0700

    style: reformatted to PEP8

--- resources/users.py
+++ resources/users.py
@@ -1,6 +1,7 @@
 from flask import request                   # import flask request
 from flask_restful import Resource          # import resource from restful
-from marshmallow import ValidationError     # import class to raise errors for marshmallow
+# import class to raise errors for marshmallow
+from marshmallow import ValidationError
 
 from models.users import UserModel          # import model for user
 from schemas.users import UserSchema        # import schema for user
@@ -10,7 +11,7 @@ from schemas.users import UserSchema        # import schema for user
 class UserRegister(Resource):
     # Function to create a new user
     @classmethod
-    def post(self):
+    def post(cls):
         data = request.json                     # fetch the info of the request
         user_schema = UserSchema()              # declare the schema for user
         try:
@@ -18,13 +19,14 @@ class UserRegister(Resource):
         except ValidationError as err:
             return err.messages
 
-        if UserModel.find_by_username(data['username']):            # if username already existed, return 400
+        if UserModel.find_by_username(data['username']):
+            # if username already existed, return 400
             return {'message': 'Username already exists'}, 400
 
-        user = UserModel(**data)                # username available, create a new user
+        user = UserModel(**data)     # username available, create a new user
 
         try:
-            user.save_to_db()                   # save the user to the database
+            user.save_to_db()        # save the user to the database
         except:
             return {'message': 'Internal error, user not created'}, 500
 
