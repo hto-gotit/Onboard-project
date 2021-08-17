65ef4058786499e55409a6cbe6346cc105fbcc49
hto-gotit <88703082+hto-gotit@users.noreply.github.com>
2021-08-17 17:40:15 +0700

    feat: Created production configuration

---
+++ config/config_production.py
@@ -0,0 +1,8 @@
+mySQLusername = 'root'
+mySQLpassword = 'Password1!'
+host = 'localhost'
+dbname = 'catalogdb'
+trackModification = False
+secretKey = 'SecRetPassWord123*()'
+port = 5000
+debug = False
