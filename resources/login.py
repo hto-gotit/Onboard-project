002ca35901ffb38ad4a25985c9c9da94f7e8a24f
hto-gotit <88703082+hto-gotit@users.noreply.github.com>
2021-08-17 09:58:51 +0700

    feat: Added decorator to take request and validate

--- resources/login.py
+++ resources/login.py
@@ -1,51 +1,30 @@
-# import flask request
-from flask import request
-# import needed jwt functions
+# needed jwt functions
 from flask_jwt_extended import create_access_token
-# import resource from restful
+# resource from restful
 from flask_restful import Resource
-# import bcrypt for security use
-import bcrypt
 
-from models.users import UserModel            # import user models
+# user model
+from models.users import UserModel
+# function for verifying given password and decorator for validating request
+from utilities import verify_password, user_request_validate
 
 
 # /auth/login resource
 class UserLogin(Resource):
     # Function to log the user in
     @classmethod
-    def post(cls):
-        data = request.json       # fetch the request data
+    @user_request_validate
+    def post(cls, data):
         if 'username' not in data:
             # return 400 if the request does not have username key
             return {'message': 'Lacking field username'}, 400
         # get the user from database
         user = UserModel.find_by_username(data['username'])
-        if not user:
-            # return 400 if username is invalid
-            return {'message': 'Invalid username'}, 400
-        try:
-            if user and verify_password(data['password'], user.password):
-                # if user exists and password is correct, then
-                # return a jwt token
-                try:
-                    access_token = create_access_token(identity=user.id,
-                                                       fresh=True)
-                    return {'access_token': access_token}, 200
-                except:
-                    return {'message': 'Login unsuccessful'}, 500
-            # Otherwise, password is wrong
-        except ValueError:
-            return {'message': 'Invalid password'}, 400
-        return {'message': 'Invalid password'}, 400
-
-
-# Function to verify user request password for login
-# and hashed password in the database
-def verify_password(password, database_hash):
-    # encode the request password
-    pass_bytes = password.encode()
-    # encode the database hashed password
-    hash_bytes = database_hash.encode()
-    # return true if they are the same, false otherwise
-    return bcrypt.checkpw(pass_bytes, hash_bytes)
+        if user and verify_password(data['password'], user.password):
+            # if user exists and password is correct, then
+            # return a jwt token
+            access_token = create_access_token(identity=user.id,
+                                               fresh=True)
+            return {'access_token': access_token}, 200
+        # Otherwise, password is wrong
+        return {'message': 'Invalid username or password'}, 400
