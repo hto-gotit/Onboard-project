098575c1e84a836b35abd6708182bfcab014c32a
hto-gotit <88703082+hto-gotit@users.noreply.github.com>
2021-08-18 12:38:58 +0700

    feat: Removed using before_first_request and command-args, Added config using ENV variable

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-# SQLAlchemy exceptions
-import sqlalchemy.exc
 # Flask class & json functions
 from flask import Flask, json
 # restful API
@@ -8,8 +6,6 @@ from flask_restful import Api
 from flask_jwt_extended import JWTManager
 # Http exceptions
 from werkzeug.exceptions import HTTPException
-# Handle command line arguments
-import argparse
 
 # resource to register
 from resources.users import UserRegister
@@ -19,48 +15,20 @@ from resources.login import UserLogin
 from resources.items import Item, CategoryItems, AllItems
 # resource to get all categories
 from resources.categories import CategoryList
-# category models to create default category
-from models.categories import CategoryModel
 
 # Instantiate the Flask application
 app = Flask(__name__)
-# Commandline argument parser
-ap = argparse.ArgumentParser()
-ap.add_argument("-env", "--environment",
-                required=True,
-                help="sepcify the environment of the app: "
-                     "test, develop, or production")
-args = vars(ap.parse_args())
 
-# Use configuration according to environment
-if args['environment'] == 'test':
-    from config import config_test
-    configFile = config_test
-elif args['environment'] == 'develop':
-    from config import config_develop
-    configFile = config_develop
-else:
-    from config import config_production
-    configFile = config_production
+# Get app configuration
+app.config.from_envvar('ENV')
 
-# database credential
-app.config['SQLALCHEMY_DATABASE_URI'] \
-    = 'mysql+pymysql://{}:{}@{}/{}'.format(configFile.mySQLusername,
-                                           configFile.mySQLpassword,
-                                           configFile.host,
-                                           configFile.dbname)
-# Track modifications
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']\
-    = configFile.trackModification
-# Secret key for app
-app.secret_key = configFile.secretKey
 # Instantiate the API
 api = Api(app)
 
 
 # Handle HTTP Exceptions
 @app.errorhandler(HTTPException)
-def handle_exception(e):
+def handle_http_exception(e):
     response = e.get_response()
     response.data = json.dumps({
         'status code': e.code,
@@ -73,24 +41,8 @@ def handle_exception(e):
 
 # Handle all Exceptions
 @app.errorhandler(Exception)
-def internal_server_error_500(e):
-    return {'message': 'Internal Server Error', 'error': e.args[0]}, 500
-
-
-@app.before_first_request               # Create all tales in database
-def create_tables():
-    db.drop_all()                   # drop all past tables
-    db.create_all()                 # create all tables
-    # Create some default category
-    try:
-        cat1 = CategoryModel("First category")
-        cat2 = CategoryModel("Second category")
-        cat3 = CategoryModel("Third category")
-        cat1.save_to_db()
-        cat2.save_to_db()
-        cat3.save_to_db()
-    except sqlalchemy.exc.IntegrityError:
-        pass
+def default_handler(e):
+    return {'message': 'Internal Server Error'}, 500
 
 
 # Instantiate the JWT manager
@@ -98,12 +50,10 @@ jwt = JWTManager(app)
 
 # Declare all the Resources for the API
 api.add_resource(CategoryList, '/categories')
-# api.add_resource(Category, '/categories/<int:category_id>')
 api.add_resource(CategoryItems, '/categories/<int:category_id>/items')
 api.add_resource(Item, '/categories/<int:category_id>/items/<int:item_id>')
 api.add_resource(AllItems, '/items')
 api.add_resource(UserRegister, '/users')
-# api.add_resource(User, '/user/<int:user_id>')
 api.add_resource(UserLogin, '/auth/login')
 
 # if running in main
@@ -113,4 +63,4 @@ if __name__ == '__main__':
     # Instantiate the database
     db.init_app(app)
     # Run application
-    app.run(port=configFile.port, debug=configFile.debug)
+    app.run()
