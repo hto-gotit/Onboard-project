f88f71ee8701540ee3442b66f75ef49f364905ae
hto-gotit <88703082+hto-gotit@users.noreply.github.com>
2021-08-18 12:49:36 +0700

    feat: Changed production configuration to cfg file

--- config/config_production.py
+++
@@ -1,8 +0,0 @@
-mySQLusername = 'root'
-mySQLpassword = 'Password1!'
-host = 'localhost'
-dbname = 'catalogdb'
-trackModification = False
-secretKey = 'SecRetPassWord123*()'
-port = 5000
-debug = False
