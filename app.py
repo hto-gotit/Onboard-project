49e98cdb59ae54fb1a7000bc93ea55660f50be0f
hto-gotit <88703082+hto-gotit@users.noreply.github.com>
2021-08-19 12:56:30 +0700

    feat: Added registration for custom errors

--- app.py
+++ app.py
@@ -15,6 +15,8 @@ from resources.login import UserLogin
 from resources.items import Item, CategoryItems, AllItems
 # resource to get all categories
 from resources.categories import CategoryList
+# custom errors
+from errors import *
 
 # Instantiate the Flask application
 app = Flask(__name__)
@@ -25,25 +27,17 @@ app.config.from_envvar('ENV')
 # Instantiate the API
 api = Api(app)
 
+# Register custom errors handler
+app.register_error_handler(CategoryDNE, handle_custom_errors)
+app.register_error_handler(ItemDNE, handle_custom_errors)
+app.register_error_handler(UserForbidden, handle_custom_errors)
+app.register_error_handler(ItemNameDuplicate, handle_custom_errors)
+app.register_error_handler(InvalidCredentials, handle_custom_errors)
+app.register_error_handler(UsernameAlreadyExists, handle_custom_errors)
 
-# Handle HTTP Exceptions
-@app.errorhandler(HTTPException)
-def handle_http_exception(e):
-    response = e.get_response()
-    response.data = json.dumps({
-        'status code': e.code,
-        'name': e.name,
-        'description': e.description,
-    })
-    response.content_type = "application/json"
-    return response
-
-
-# Handle all Exceptions
-@app.errorhandler(Exception)
-def default_handler(e):
-    return {'message': 'Internal Server Error'}, 500
-
+# Register HTTP errors handler and default error handler
+app.register_error_handler(HTTPException, handle_http_exception)
+app.register_error_handler(Exception, default_handler)
 
 # Instantiate the JWT manager
 jwt = JWTManager(app)
