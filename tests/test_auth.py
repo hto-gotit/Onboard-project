819778eb200e98f29fc5820e0fd1fb95098204b1
hto-gotit <88703082+hto-gotit@users.noreply.github.com>
2021-08-19 13:54:19 +0700

    feat: Changed tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,36 +1,9 @@
-import pytest
-from flask_restful import Api
-from flask_jwt_extended import JWTManager
 import json
-from flask import Flask
-
-from models.users import UserModel
-from models.items import ItemModel
-from models.categories import CategoryModel
-from db import db
-
-from resources.users import UserRegister
-from resources.login import UserLogin
+import pytest
+from errors import *
 
 
-@pytest.fixture(autouse=True)
-def client():
-    app = Flask(__name__)
-    app.config.from_envvar('ENV')
-    client = app.test_client()
-    db.init_app(app)
-    with app.app_context():
-        db.drop_all()
-        db.create_all()
-
-    api = Api(app)
-    api.add_resource(UserRegister, '/users')
-    api.add_resource(UserLogin, '/auth/login')
-    jwt = JWTManager(app)
-    return client
-
-
-def help_user(username, password, url, client):
+def register_login_user(username, password, url, client):
     response = client.post(url,
                            data=json.dumps(dict(username=username,
                                                 password=password)),
@@ -40,49 +13,43 @@ def help_user(username, password, url, client):
 
 def test_register_user_success(client):
     url = '/users'
-    response = help_user('user1', 'pass1', url, client)
-    # response = client.post(url,
-    #                        data=json.dumps(dict(username='user1',
-    #                                             password='pass1')),
-    #                        content_type='application/json')
+    response = register_login_user('user1', 'pass1', url, client)
     assert response.get_json() == {'message': 'User created successfully'}
     assert response.status_code == 201
 
 
-def test_register_user_duplicate_name(client):
+def test_failed_register_user_duplicate_name(client, database):
     url = '/users'
-    response = help_user('user1', 'pass1', url, client)
+    response = register_login_user('user1', 'pass1', url, client)
     assert response.status_code == 201
-    response = help_user('user1', 'pass1', url, client)
-    assert response.status_code == 400
-    assert response.get_json()['description'] == 'Username already exists'
+    with pytest.raises(UsernameAlreadyExists):
+        register_login_user('user1', 'pass1', url, client)
+    assert 2 == 2
 
 
 def test_login_success(client):
     url = '/users'
-    response = help_user('user1', 'pass1', url, client)
+    response = register_login_user('user1', 'pass1', url, client)
     assert response.status_code == 201
     url2 = '/auth/login'
-    response = help_user('user1', 'pass1', url2, client)
+    response = register_login_user('user1', 'pass1', url2, client)
     assert 'access_token' in response.get_json()
     assert response.status_code == 200
 
 
-def test_login_invalid_username(client):
+def test_failed_login_invalid_username(client):
     url = '/users'
-    response = help_user('user1', 'pass1', url, client)
+    response = register_login_user('user1', 'pass1', url, client)
     assert response.status_code == 201
     url2 = '/auth/login'
-    response = help_user('user1111', 'pass1', url2, client)
-    assert response.get_json()['description'] == 'Invalid username or password'
-    assert response.status_code == 400
+    with pytest.raises(InvalidCredentials):
+        register_login_user('user1111', 'pass1', url2, client)
 
 
-def test_login_invalid_password(client):
+def test_failed_login_invalid_password(client):
     url = '/users'
-    response = help_user('user1', 'pass1', url, client)
+    response = register_login_user('user1', 'pass1', url, client)
     assert response.status_code == 201
     url2 = '/auth/login'
-    response = help_user('user1', 'wrongpassword', url2, client)
-    assert response.get_json()['description'] == 'Invalid username or password'
-    assert response.status_code == 400
+    with pytest.raises(InvalidCredentials):
+        register_login_user('user1', 'wrongpassword', url2, client)
