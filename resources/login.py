9f3df3573f269c16272d1f02bc5c9c2a4488270c
hto-gotit <88703082+hto-gotit@users.noreply.github.com>
2021-08-19 12:53:37 +0700

    feat: Added custom error instead of abort

--- resources/login.py
+++ resources/login.py
@@ -1,27 +1,30 @@
 # needed jwt functions
 from flask_jwt_extended import create_access_token
 # resource and errors handling from restful
-from flask_restful import Resource, abort
+from flask_restful import Resource
 
 # user model
 from models.users import UserModel
+# user schema
+from schemas.users import UserSchema
 # function for verifying given password and decorator for validating request
-from utilities import verify_password, user_request_validate
+from utilities import verify_password, validate
+# custom errors
+from errors import InvalidCredentials
 
 
 # /auth/login resource
 class UserLogin(Resource):
     # Function to log the user in
     @classmethod
-    @user_request_validate
+    @validate(UserSchema())
     def post(cls, data):
         # get the user from database
         user = UserModel.find_by_username(data['username'])
         if user and verify_password(data['password'], user.password):
             # if user exists and password is correct, then
             # return a jwt token
-            access_token = create_access_token(identity=user.id,
-                                               fresh=True)
-            return {'access_token': access_token}, 200
+            access_token = create_access_token(identity=user.id)
+            return {'access_token': access_token}
         # Otherwise, password is wrong
-        abort(400, description='Invalid username or password')
+        raise InvalidCredentials()
