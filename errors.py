11c55e3ce2b8a7991443ef6f77d5058868afcc2a
hto-gotit <88703082+hto-gotit@users.noreply.github.com>
2021-08-20 12:49:19 +0700

    feat: Added another handler for error 401 not logged in

--- errors.py
+++ errors.py
@@ -1,32 +1,41 @@
-class CategoryDNE(Exception):
+class ParentException(Exception):
+    pass
+
+
+class CategoryDoesNotExist(ParentException):
     code = 404
     message = 'Category not found'
 
 
-class ItemDNE(Exception):
+class ItemDoesNotExist(ParentException):
     code = 404
     message = 'Item not found'
 
 
-class ItemNameDuplicate(Exception):
+class ItemNameDuplicate(ParentException):
     code = 400
     message = 'Item name already exists in destination category'
 
 
-class InvalidCredentials(Exception):
+class InvalidCredentials(ParentException):
     code = 400
     message = 'Invalid username or password'
 
 
-class UsernameAlreadyExists(Exception):
+class UsernameAlreadyExists(ParentException):
     code = 400
     message = 'Username already exists'
 
 
-class UserForbidden(Exception):
+class UserForbidden(ParentException):
     code = 403
     message = 'User is forbidden to perform this action ' \
               '(user is not item creator)'
+
+
+class ValidateSchemaError(ParentException):
+    code = 400
+    message = 'Something is wrong in the request'
 
 
 # handle custom errors
@@ -39,6 +48,10 @@ def handle_http_exception(e):
     return {'message': e.description}, e.code
 
 
+def handle_missing_jwt(e):
+    return {'message': 'You are not logged in'}, 401
+
+
 # Handle all Exceptions
-def default_handler():
+def default_handler(*args, **kwargs):
     return {'message': 'Internal Server Error'}, 500
