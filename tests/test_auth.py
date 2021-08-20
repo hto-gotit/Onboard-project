526dd77e33b377f2b8ec8029fac31a966baa8f2f
hto-gotit <88703082+hto-gotit@users.noreply.github.com>
2021-08-20 12:51:01 +0700

    feat: Changed tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,55 +1,31 @@
-import json
-import pytest
-from errors import *
+from conftest import register_or_login_user
 
 
-def register_login_user(username, password, url, client):
-    response = client.post(url,
-                           data=json.dumps(dict(username=username,
-                                                password=password)),
-                           content_type='application/json')
-    return response
-
-
-def test_register_user_success(client):
-    url = '/users'
-    response = register_login_user('user1', 'pass1', url, client)
+def test_register_new_user_successfully(client):
+    response = register_or_login_user('user1', 'pass1', 'register', client)
     assert response.get_json() == {'message': 'User created successfully'}
     assert response.status_code == 201
 
 
-def test_failed_register_user_duplicate_name(client, database):
-    url = '/users'
-    response = register_login_user('user1', 'pass1', url, client)
-    assert response.status_code == 201
-    with pytest.raises(UsernameAlreadyExists):
-        register_login_user('user1', 'pass1', url, client)
-    assert 2 == 2
+def test_failed_to_register_new_user_duplicate_name(client):
+    response = register_or_login_user('user1', 'pass1', 'register', client)
+    assert response.status_code == 400
+    assert response.get_json() == {'message': 'Username already exists'}
 
 
 def test_login_success(client):
-    url = '/users'
-    response = register_login_user('user1', 'pass1', url, client)
-    assert response.status_code == 201
-    url2 = '/auth/login'
-    response = register_login_user('user1', 'pass1', url2, client)
+    response = register_or_login_user('user1', 'pass1', 'login', client)
     assert 'access_token' in response.get_json()
     assert response.status_code == 200
 
 
-def test_failed_login_invalid_username(client):
-    url = '/users'
-    response = register_login_user('user1', 'pass1', url, client)
-    assert response.status_code == 201
-    url2 = '/auth/login'
-    with pytest.raises(InvalidCredentials):
-        register_login_user('user1111', 'pass1', url2, client)
+def test_failed_login__with_invalid_username(client):
+    response = register_or_login_user('user1111', 'pass1', 'login', client)
+    assert response.status_code == 400
+    assert response.get_json() == {'message': 'Invalid username or password'}
 
 
-def test_failed_login_invalid_password(client):
-    url = '/users'
-    response = register_login_user('user1', 'pass1', url, client)
-    assert response.status_code == 201
-    url2 = '/auth/login'
-    with pytest.raises(InvalidCredentials):
-        register_login_user('user1', 'wrongpassword', url2, client)
+def test_failed_login_with_invalid_password(client):
+    response = register_or_login_user('user1', 'wrongpassword', 'login', client)
+    assert response.status_code == 400
+    assert response.get_json() == {'message': 'Invalid username or password'}
