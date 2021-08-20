8b459972f861cb0945e346876614ff726c2d08d4
hto-gotit <88703082+hto-gotit@users.noreply.github.com>
2021-08-20 12:52:21 +0700

    feat: Imported and used app from app.py, make app and db scope to session, added check 'env' variable, redid helper functions

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,60 +1,50 @@
 import pytest
-from flask_restful import Api
-from flask_jwt_extended import JWTManager
 import json
-from flask import Flask
+import os
+import random
+import string
 
 from models.categories import CategoryModel
 from db import db
-from resources.items import Item, AllItems, CategoryItems
-from resources.users import UserRegister
-from resources.login import UserLogin
-from resources.categories import CategoryList
+from app import app
 
 
-@pytest.fixture(autouse=True)
-def app():
-    app = Flask(__name__)
-    app.config.from_envvar('ENV')
-    api = Api(app)
-    JWTManager(app)
-    api.add_resource(CategoryItems, '/categories/<int:category_id>/items')
-    api.add_resource(Item, '/categories/<int:category_id>/items/<int:item_id>')
-    api.add_resource(AllItems, '/items')
-    api.add_resource(UserRegister, '/users')
-    api.add_resource(UserLogin, '/auth/login')
-    api.add_resource(CategoryList, '/categories')
-    return app
-
-
-@pytest.fixture(autouse=True)
-def database(app):
+@pytest.fixture(scope='session')
+def application():
+    if os.environ['ENV'] != 'config\\test.cfg':
+        pytest.exit("Wrong Environment!")
     db.init_app(app)
     with app.app_context():
-        db.session.close()
         db.drop_all()
         db.create_all()
         CategoryModel("First category").save_to_db()
         CategoryModel("Second category").save_to_db()
+    return app
 
 
 @pytest.fixture()
-def client(app):
-    client = app.test_client()
+def client(application):
+    client = application.test_client()
     return client
 
 
 @pytest.fixture()
-def login_helper(client):
-    url = '/users'
-    client.post(url,
-                data=json.dumps(dict(username='user1',
-                                     password='pass1')),
-                content_type='application/json')
-    url = '/auth/login'
-    response = client.post(url,
-                           data=json.dumps(dict(username='user1',
-                                                password='pass1')),
-                           content_type='application/json')
+def get_access_token(client):
+    username = ''.join(random.choices(string.ascii_letters, k=4))
+    password = ''.join(random.choices(string.ascii_letters, k=4))
+    register_or_login_user(username, password, 'register', client)
+    response = register_or_login_user(username, password, 'login', client)
     access_token = response.get_json()['access_token']
     return access_token
+
+
+def register_or_login_user(username, password, choice, client):
+    if choice == 'login':
+        url = '/auth/login'
+    else:
+        url = '/users'
+    response = client.post(url,
+                           data=json.dumps(dict(username=username,
+                                                password=password)),
+                           content_type='application/json')
+    return response
