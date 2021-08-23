3764914cf60bb9b6cf9652a46374fa6c56ae3a44
hto-gotit <88703082+hto-gotit@users.noreply.github.com>
2021-08-23 09:51:38 +0700

    refactor: changed test names

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -7,25 +7,25 @@ def test_register_new_user_successfully(client):
     assert response.status_code == 201
 
 
-def test_failed_to_register_new_user_duplicate_name(client):
+def test_failed_to_register_new_user_with_a_duplicate_name(client):
     response = register_or_login_user('user1', 'pass1', 'register', client)
     assert response.status_code == 400
     assert response.get_json() == {'message': 'Username already exists'}
 
 
-def test_login_success(client):
+def test_login_successfully(client):
     response = register_or_login_user('user1', 'pass1', 'login', client)
     assert 'access_token' in response.get_json()
     assert response.status_code == 200
 
 
-def test_failed_login__with_invalid_username(client):
+def test_failed_to_login_with_invalid_username(client):
     response = register_or_login_user('user1111', 'pass1', 'login', client)
     assert response.status_code == 400
     assert response.get_json() == {'message': 'Invalid username or password'}
 
 
-def test_failed_login_with_invalid_password(client):
+def test_failed_to_login_with_invalid_password(client):
     response = register_or_login_user('user1', 'wrongpassword', 'login', client)
     assert response.status_code == 400
     assert response.get_json() == {'message': 'Invalid username or password'}
