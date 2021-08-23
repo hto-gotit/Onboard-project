a64bc9a992ca6a41f6ebb7cc4331adfb32a93806
hto-gotit <88703082+hto-gotit@users.noreply.github.com>
2021-08-23 09:14:46 +0700

    feat: Added constants

--- schemas/categories.py
+++ schemas/categories.py
@@ -1,5 +1,7 @@
 # import Marshmallow
-from marshmallow import Schema, fields, validates, ValidationError
+from marshmallow import Schema, fields, validate
+# import constants
+from config.constants import *
 
 
 # Declare the Schema for Category, which consists of category id and its name
@@ -7,13 +9,8 @@ class CategorySchema(Schema):
     # id of the category
     id = fields.Integer()
     # name of the category, it is required
-    name = fields.String(required=True)
-
-    # function to validate the name of the category
-    @validates("name")
-    def validate_username(self, value):
-        # name length must be in the range of [1,19]
-        if len(value) < 1:
-            raise ValidationError("Category name's length must be > 0.")
-        if len(value) > 19:
-            raise ValidationError("Category name's length must be < 20.")
+    name = fields.String(required=True,
+                         validate=validate.Length(
+                             min=min_string_length,
+                             max=max_category_name_length)
+                         )
