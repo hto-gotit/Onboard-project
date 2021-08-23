ff6d42077c9cc2eb9ffd20e2d33e55663698a5cd
hto-gotit <88703082+hto-gotit@users.noreply.github.com>
2021-08-23 09:13:44 +0700

    feat: Added pre_load method to strip whitespaces and used constants

--- schemas/users.py
+++ schemas/users.py
@@ -1,32 +1,29 @@
 # import Marshmallow
-from marshmallow import Schema, fields, ValidationError, validates
+from marshmallow import Schema, fields, validate, pre_load
+# import constants
+from config.constants import *
 
 
 # Declare the Schema for User, which consists of user's username and password
 class UserSchema(Schema):
-    username = fields.String(required=True)         # username of the account
-    password = fields.String(required=True)         # password of the account
+    # username of the account
+    username = fields.String(required=True,
+                             validate=validate.Length(
+                                 min=min_string_length,
+                                 max=max_user_string_length)
+                             )
 
-    # Function to validate the username of the account
-    @validates("username")
-    def validate_username(self, value):
-        # The username must be a string with length in range [1,79]
-        if len(value) < 1:
-            raise ValidationError("Username's length must be > 0.")
-        if len(value) > 79:
-            raise ValidationError("Username's length must be < 80.")
-        if value[0] == ' ' or value[-1] == ' ':
-            raise ValidationError("Username cannot have empty space in the "
-                                  "beginning and the end")
+    # password of the account
+    password = fields.String(required=True,
+                             validate=validate.Length(
+                                 min=min_string_length,
+                                 max=max_user_string_length)
+                             )
 
-    # Function to validate the password of the account
-    @validates("password")
-    def validate_password(self, value):
-        # The password must be a string with length in range [1,79]
-        if len(value) < 1:
-            raise ValidationError("Password's length must be > 0.")
-        if len(value) > 79:
-            raise ValidationError("Password's length must be < 80.")
-        if value[0] == ' ' or value[-1] == ' ':
-            raise ValidationError("Username cannot have space in the "
-                                  "beginning and the end")
+    @pre_load
+    def strip_whitespace(self, in_data, **kwargs):
+        if all(key in in_data and type(in_data[key]) == str
+               for key in ('username', 'password')):
+            in_data['username'] = in_data['username'].strip()
+            in_data['password'] = in_data['password'].strip()
+        return in_data
